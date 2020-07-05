a95674773b8536004d50535ca5af01d3492bc4e1
Ravikumar Chintakayala <rchintakayala@talentsoft.com>
2020-07-05 10:47:50 +0200

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Ravikumar Chintakayala on 05/07/2020.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
